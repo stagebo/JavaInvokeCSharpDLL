01a5962d7bc87b2aa7830131f80e99cbd6e94b9e
stagebo <1254117589@qq.com>
2017-09-05 15:13:24 +0800

    2017-9-5 15:13:19

--- DllTest02/main.cpp
+++ DllTest02/main.cpp
@@ -12,16 +12,8 @@
 
 
 MYLIBAPI char* Do(char* connStr, char* dept, char* gchid);
+MYLIBAPI int add(int a, int b);
 
-
-
-
-
-
-
-MYLIBAPI char* Get(char* str);
-
-MYLIBAPI int OutTest();
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 //	main.cpp文件
 //	2017-8-23 16:44:06
@@ -39,9 +31,6 @@ using namespace TDYH;
 
 char* Do(char* connStr, char* dept, char* gchid)
 {
-	return "this is c++ tip.";
-	cout << "this is c++ program" << endl;
-
 	TDYHDecorator ^tdyh;
 	tdyh =gcnew TDYHDecorator();
 	String ^connString = gcnew String(connStr);
@@ -49,11 +38,11 @@ char* Do(char* connStr, char* dept, char* gchid)
 	String ^gchidString = gcnew String(gchid);
 	String ^ result = tdyh->Do(connString,deptString,gchidString);
 	char* out = (char*)(void*)System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(result);
-
-	//String ^resultString = tdyhDecorator->Do();
-	//char* resultChar = (char*)(void*)System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(resultString);
-	//char * resultChar = (char*)(void*)System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(connString);
-	return out;//"this is win32 c++ test result.\r\n";
+	return out;
 }
 
+int add(int a, int b)
+{
+	return a + b;
+}
 /*****************************************函数体End******************************************************************/
--- Win32Test/Win32Test.cpp
+++ Win32Test/Win32Test.cpp
@@ -6,12 +6,14 @@
 using namespace std;
 using namespace TDYH;
 using namespace System;
-using namespace LibDemo;
 
 int main()
 {
-	Function ^ f = gcnew Function();
-	printf_s("c# result:%d",f->intTest(3));
+	TDYHDecorator ^ t = gcnew TDYHDecorator();
+	String^ re = t->Do(gcnew String(""), gcnew String(""), gcnew String(""));
+	char * result = (char*)(void*)System::Runtime::InteropServices::Marshal::StringToCoTaskMemAnsi(re);
+	cout << endl;
+	printf_s("the result is :%s",result);
 	while (1);
 }
 
